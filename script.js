44d62ec8428fbd77d391e9a12a9b5c03819e3b95
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-09-14 21:02:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,50 +1,43 @@
 const container = document.getElementById("container");
 
-        const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong", "stop"];
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong", "stop"];
+const playList = {};
+const tempDom = document.createDocumentFragment();
 
-        const tempDom = document.createDocumentFragment();
-        for (let sound of sounds) {
-            createButton(sound);
-        }
-        container.appendChild(tempDom);
+for (let sound of sounds) {
+    createButton(sound);
+}
 
-        const playList = {}
+container.appendChild(tempDom);
 
+function createButton(sound) {
+    const button = document.createElement("button");
+    button.innerText = sound;
+    button.id = sound;
+    button.className = "btn" + (sound === "stop" ? " stop" : "");
+    tempDom.appendChild(button);
 
-        function createButton(sound) {
-            const button = document.createElement("button");
-            button.innerText = sound;
-            button.id = sound;
-            button.className = "btn" + (sound === "stop" ? " stop" : "");
-            tempDom.appendChild(button);
+    button.addEventListener("click", () => {
+        if (sound === "stop") {
+            stopSounds();
+        } else {
+            stopSounds(); // Stop others before playing
 
-
-            button.addEventListener("click", () => {
-                if (sound === "stop") {
-                    stopSounds();
-                } else {
-                    stopSounds(); // Stop others before playing
-
-                    if (playList.hasOwnProperty(sound)) {
-                        console.log(playList,'play list in if condition')
-                        playList[sound].play()
-                    }
-                    else {
-                        const audio = new Audio(`sounds/${sound}.mp3`);
-                        console.log(audio.currentTime, 'audio played secs')
-                        playList[sound] = audio
-                        audio.play();
-                        console.log("new created")
-                    }
-
-                }
-            });
-        }
-
-
-        function stopSounds() {
-            for (let key in playList) {
-                const audio=playList[key];
-                audio.pause();
+            if (playList.hasOwnProperty(sound)) {
+                playList[sound].play();
+            } else {
+                const audio = new Audio(`sounds/${sound}.mp3`);
+                playList[sound] = audio;
+                audio.play();
             }
         }
+    });
+}
+
+function stopSounds() {
+    for (let key in playList) {
+        const audio = playList[key];
+        audio.pause();
+        audio.currentTime = 0; // reset to beginning
+    }
+}
